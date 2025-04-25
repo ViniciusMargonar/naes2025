8ee9a57c2fae0ea13f6263121c4008c6c2f202df
Vinícius Margonar <vinimargonar@hotmail.com.br>
2025-04-24 21:46:31 -0300

    Urls para as Create Views

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
-from .views import View1, View2, View3, etc #importar views CREATE aqui
+from .views import CategoriaItemCreate, CidadeCreate, EstadoCreate, FornecedorCreate, FrotaCreate, ItemCreate, ItemPedidoCreate, PedidoCreate, View1, View2, View3, etc #importar views CREATE aqui
 from .views import View1, View2, View3, etc  # Importe views UPDATE aqui
 
 urlpatterns = [
     #VIEWS CREATE
-    path('cadastrar/view1/', View1.as_view(), name='view1-campus'),
-    path('cadastrar/view2/', View2.as_view(), name='view2-curso'),
-    path('cadastrar/view3/', View3.as_view(), name='view3-status'),
+    path('cadastrar/estado/', EstadoCreate.as_view(), name='cadastrar-estado'),
+    path('cadastrar/cidade/', CidadeCreate.as_view(), name='cadastrar-cidade'),
+    path('cadastrar/fornecedor/', FornecedorCreate.as_view(), name='cadastrar-fornecedor'),
+    path('cadastrar/frota/', FrotaCreate.as_view(), name='cadastrar-frota'),
+    path('cadastrar/categoriaitem/', CategoriaItemCreate.as_view(), name='cadastrar-categoriaitem'),
+    path('cadastrar/item/', ItemCreate.as_view(), name='cadastrar-item'),
+    path('cadastrar/itempedido/', ItemPedidoCreate.as_view(), name='cadastrar-itempedido'),
+    path('cadastrar/pedido/', PedidoCreate.as_view(), name='cadastrar-pedido'),
     # Adicione mais URLs conforme necessário
 
     #VIEW UPDATE
